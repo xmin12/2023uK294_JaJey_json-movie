54f9cd5e0b0ea0ae9ae04c376653e4be319e8df1
xmin12 <112725311+xmin12@users.noreply.github.com>
2023-06-02 09:10:50 +0200

    Strated projekt with login

---
+++ movie/src/Login.tsx
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+
+
+
+function Login() {
+    interface Test {
+        email: string;
+        pass: string;
+    }
+
+    const handleSubmit = () => {
+
+        const data = {
+
+            email: "nithu@mail.com",
+            password: "1313"
+        
+        };
+
+        console.log(data)
+        const ax = axios.create();
+        ax.post("http://localhost:3030/login",data)
+            .then(response => {
+                console.log(response)
+                console.log(response.data.accessToken)
+                localStorage.setItem("token", response.data.accessToken);
+     // window.location.href = 'http://localhost:3030/movies' 
+    }) 
+     .catch(err => console.log(err));
+    }; return (
+        <div>
+            <form>
+                <label>
+                    Email:&nbsp;&emsp;&emsp;&nbsp;&nbsp;<input type="email" name="email" />
+                </label>
+                <br />
+                <label>
+                    Password:&emsp;<input type="password" name="password" />
+                </label>
+                <br />
+                <label>
+                &emsp;&emsp;&nbsp;&emsp; &emsp; &emsp; &emsp;
+                    <Button variant="contained" endIcon={<SendIcon />} onClick= {handleSubmit}> 
+                Send </Button>
+                </label>
+            </form>
+        </div>
+        );
+}
+export default Login;
